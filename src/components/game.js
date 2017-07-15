35e8d96a5c60f81ec82659a3e521d73ed77a61ef
CHARLOTTE Frates <charlottefrates@gmail.com>
2017-07-14 23:47:44 -0700

    starting to change components to have state

--- src/components/game.js
+++ src/components/game.js
@@ -1,25 +1,33 @@
 import React from 'react';
 
+import What from './game-what';
+
 import './game.css';
 
-export default function Game() {
-    return (
-        <div className="directions">
-            <div className="overlay" id="modal">
-                <div className="content">
-                        <h3>What do I do?</h3>
-                        <div>
-                            <p>This is a Hot or Cold Number Guessing Game. The game goes like this: </p>
-                            <ul>
-                                <li>1. I pick a <strong>random secret number</strong> between 1 to 100 and keep it hidden.</li>
-                                <li>2. You need to <strong>guess</strong> until you can find the hidden secret number.</li>
-                                <li>3. You will <strong>get feedback</strong> on how close ("hot") or far ("cold") your guess is.</li>
-                            </ul>
-                            <p>So, Are you ready?</p>
-                            <a className="close" href="#">Got It!</a>
-                        </div>
-                </div>
-            </div>
-        </div>
-    );
+
+export default class Game extends React.Component {
+     //specify state
+     constructor(props) {
+          super(props);
+          // have current state to show directions
+          this.state = {
+               show: 'directions'
+          }
+     }
+
+     //function to change state
+  toogleimage(){
+        this.setState({
+            show: 'game'
+        });
+    }
+
+
+     render() {
+
+          return (
+            <What />
+          );
+     }
+
 }
